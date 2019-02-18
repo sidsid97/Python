2ddb0d21ad3d0d127de09aa792b9d58568e003fd
sidsid97 <47661303+sidsid97@users.noreply.github.com>
2019-02-18 09:19:09 -0300

    Add files via upload

---
+++ ExercicioCpf.py
@@ -0,0 +1,19 @@
+cpf=1234
+print(cpf)
+print('O meu cpf é',cpf)
+print(type(cpf))
+
+nome='Sidney'
+telefone=45681234
+altura=1.80
+
+print('O seu nome é ',nome)
+print('O seu telefone é ',telefone)
+print('O seu cpf é ',cpf)
+print('A sua altura é',altura)
+
+print(type(nome))
+print(type(telefone))
+print(type(cpf))
+print(type(altura))
+
